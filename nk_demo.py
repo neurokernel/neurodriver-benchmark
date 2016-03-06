3e44080cee6209412327a9fff59ec3646b15bd51
Lev Givon <lev@columbia.edu>
2016-03-05 21:42:09 -0500

    Generate NK circuit within nk_demo.py without writing it to disk to save time.

--- nk_demo.py
+++ nk_demo.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 
-"""
-Generic LPU demo
-
-Notes
------
-Generate input file and LPU configuration by running
-
-python gen_nk_lpu.py
-"""
-
 import argparse
 import itertools
+from itertools import product
 import time
 
 import networkx as nx
+import numpy as np
 import pycuda.driver as drv
 
 from neurokernel.tools.logging import setup_logger
@@ -24,39 +16,131 @@ from neurokernel.LPU.LPU import LPU
 
 import neurokernel.mpi_relaunch
 
-dt = 1e-4
-dur = 3.0
-steps = int(dur/dt)
+def create_lpu_graph(lpu_name, N_sensory, N_local, N_proj):
+    """
+    Create a generic LPU graph comprising spiking neurons.
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--debug', default=False,
-                    dest='debug', action='store_true',
-                    help='Write connectivity structures and inter-LPU routed data in debug folder')
-parser.add_argument('-l', '--log', default='none', type=str,
-                    help='Log output to screen [file, screen, both, or none; default:none]')
-parser.add_argument('-s', '--steps', default=steps, type=int,
-                    help='Number of steps [default: %s]' % steps)
-parser.add_argument('-g', '--gpu_dev', default=0, type=int,
-                    help='GPU device number [default: 0]')
-args = parser.parse_args()
+    Creates a graph containing the neuron and synapse parameters for an LPU
+    containing the specified number of local and projection neurons. The graph
+    also contains the parameters for a set of sensory neurons that accept
+    external input. All neurons are Leaky Integrate-and-Fire neurons, and all
+    synapses use the alpha function model.
 
-file_name = None
-screen = False
-if args.log.lower() in ['file', 'both']:
-    file_name = 'neurokernel.log'
-if args.log.lower() in ['screen', 'both']:
-    screen = True
-logger = setup_logger(file_name=file_name, screen=screen)
+    Parameters
+    ----------
+    lpu_name : str
+        Name of LPU. Used in port identifiers.
+    N_sensory : int
+        Number of sensory neurons.
+    N_local : int
+        Number of local neurons.
+    N_proj : int
+        Number of project neurons.
 
-man = core.Manager()
+    Returns
+    -------
+    g : networkx.MultiDiGraph
+        Generated graph.
+    """
 
-lpu_file = 'nk_lpu.gexf.gz'
-(n_dict, s_dict) = LPU.lpu_parser(lpu_file)
-g = nx.read_gexf(lpu_file)
-total_neurons =  \
-    len([d for n, d in g.nodes(data=True) if d['model'] == 'LeakyIAF'])
-total_synapses = \
-    len([d for f, t, d in g.edges(data=True) if d['model'] == 'AlphaSynapse'])
+    # Set numbers of neurons:
+    neu_type = ('sensory', 'local', 'proj')
+    neu_num = (N_sensory, N_local, N_proj)
+
+    # Neuron ids are between 0 and the total number of neurons:
+    G = nx.DiGraph()
+    G.add_nodes_from(range(sum(neu_num)))
+
+    idx = 0
+    spk_out_id = 0
+    for (t, n) in zip(neu_type, neu_num):
+        for i in range(n):
+            name = t+"_"+str(i)
+
+            G.node[idx] = {
+                'model': 'LeakyIAF',
+                'name': name+'_s',
+                'extern': True if t == 'sensory' else False, # True if the neuron can receive external input
+                'public': True if t == 'proj' else False,    # True if the neuron can emit output
+                'spiking': True,
+                'V': np.random.uniform(-0.06,-0.025),
+                'Vr': -0.0675489770451,
+                'Vt': -0.0251355161007,
+                'R': 1.02445570216,
+                'C': 0.0669810502993}
+
+            # Projection neurons are all assumed to be attached to output
+            # ports (which are not represented as separate nodes):
+            if t == 'proj':
+                G.node[idx]['selector'] = '/%s/out/spk/%s' % (lpu_name, str(spk_out_id))
+                G.node[idx]['circuit'] = 'proj'
+                spk_out_id += 1
+            else:
+                G.node[idx]['circuit'] = 'local'
+
+            idx += 1
+
+    # An input port node is created for and attached to each non-projection
+    # neuron with a synapse; this assumes that data propagates from one LPU to
+    # another as follows:
+    # LPU0[projection neuron] -> LPU0[output port] -> LPU1[input port] -> 
+    # LPU1[synapse] -> LPU1[non-projection neuron]
+    spk_in_id = 0
+    gpot_in_id = 0
+    for i, data in G.nodes_iter(True):
+        if data['public'] == False:
+            G.add_node(idx, {
+                'name': 'port_in_spk_%s' % spk_in_id,
+                'model': 'port_in_spk',
+                'selector': '/%s/in/spk/%s' % (lpu_name, idx),
+                'spiking': True,
+                'public': False,
+                'extern': False,
+                'circuit': G.node[i]['circuit']
+            })
+            spk_in_id += 1
+            G.add_edge(idx, i, attr_dict={
+                'name': G.node[idx]['name']+'-'+G.node[i]['name'],
+                'model': 'AlphaSynapse',
+                'class': 0,
+                'conductance': True,
+                'ad': 0.19*1000,
+                'ar': 1.1*100,
+                'gmax': 0.003,
+                'reverse': 0.065,
+                'circuit': G.node[i]['circuit']})
+
+        idx += 1
+
+    # Assume a probability of synapse existence for each group of synapses:
+    # sensory -> local, sensory -> projection, local -> projection, 
+    # projection -> local:            
+    for r, (i, j) in zip((0.5, 0.1, 0.1, 0.3),
+                         ((0, 1), (0, 2), (1, 2), (2, 1))):
+        src_off = sum(neu_num[0:i])
+        tar_off = sum(neu_num[0:j])
+
+        for src, tar in product(range(src_off, src_off+neu_num[i]),
+                                range(tar_off, tar_off+neu_num[j])):
+
+            # Don't connect all neurons:
+            if np.random.rand() > r: continue
+
+            # Connections from the sensory neurons use the alpha function model;
+            # all other connections use the power_gpot_gpot model:
+            name = G.node[src]['name'] + '-' + G.node[tar]['name']
+            G.add_edge(src, tar, attr_dict={
+                'model'       : 'AlphaSynapse',
+                'name'        : name,
+                'class'       : 0 if G.node[tar]['spiking'] is True else 1,
+                'ar'          : 1.1*1e2,
+                'ad'          : 1.9*1e3,
+                'reverse'     : 65*1e-3 if G.node[tar]['spiking'] else 0.01,
+                'gmax'        : 3*1e-3 if G.node[tar]['spiking'] else 3.1e-4,
+                'conductance' : True,
+                'circuit'     : G.node[src]['circuit']})
+
+    return G
 
 class MyLPU(LPU):
     def __init__(self, dt, n_dict, s_dict, I_const=0.6,
@@ -223,9 +307,45 @@ class MyLPU(LPU):
         self.I_ext = parray.to_gpu(np.full(self.num_input, self.I_const,
                                            np.double))
 
+dt = 1e-4
+dur = 3.0
+steps = int(dur/dt)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', default=False,
+                    dest='debug', action='store_true',
+                    help='Write connectivity structures and inter-LPU routed data in debug folder')
+parser.add_argument('-l', '--log', default='none', type=str,
+                    help='Log output to screen [file, screen, both, or none; default:none]')
+parser.add_argument('-s', '--steps', default=steps, type=int,
+                    help='Number of steps [default: %s]' % steps)
+parser.add_argument('-g', '--gpu_dev', default=0, type=int,
+                    help='GPU device number [default: 0]')
+parser.add_argument('-n', type=int, nargs=3, default=(30, 30, 30),
+                    help='Numbers of sensory, local, and projection neurons')
+args = parser.parse_args()
+
+file_name = None
+screen = False
+if args.log.lower() in ['file', 'both']:
+    file_name = 'neurokernel.log'
+if args.log.lower() in ['screen', 'both']:
+    screen = True
+logger = setup_logger(file_name=file_name, screen=screen)
+
+man = core.Manager()
+
+lpu_name = 'nk'
+g = create_lpu_graph(lpu_name, *args.n)
+n_dict, s_dict = LPU.graph_to_dicts(g)
+total_neurons =  \
+    len([d for n, d in g.nodes(data=True) if d['model'] == 'LeakyIAF'])
+total_synapses = \
+    len([d for f, t, d in g.edges(data=True) if d['model'] == 'AlphaSynapse'])
+
 output_file = 'nk_output.h5'
 
-man.add(MyLPU, 'nk', dt, n_dict, s_dict, I_const=0.6,
+man.add(MyLPU, lpu_name, dt, n_dict, s_dict, I_const=0.6,
         output_file=output_file,
         device=args.gpu_dev,
         debug=args.debug, time_sync=True)
