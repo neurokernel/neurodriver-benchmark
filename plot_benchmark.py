45e6dc58f32866cf6fcc96aec0035e024ccad9a6
Lev Givon <lev@columbia.edu>
2016-03-09 17:22:18 -0500

    Don't plot timings for Brian2GeNN-generated binary.

--- plot_benchmark.py
+++ plot_benchmark.py
@@ -27,11 +27,14 @@ df_bg_avg['synapses'] = df_bg_list[0]['synapses']
 df_bg_avg['total_time'] = sum([df['total_time'] for df in df_bg_list])/M
 df_bg_avg['exec_time'] = sum([df['exec_time'] for df in df_bg_list])/M
 
+# Plot exec_time for Neurodriver and total_time for Brian2GeNN because
+# the exec_time for Brian2GeNN contains the timings for the generated 
+# binary only without including the time taken for compilation and
+# transmitting generated data back into Python:
 plt.clf()
 plt.subplot(211)
 plt.plot(df_nd_avg['neurons'], df_nd_avg['exec_time'], 'b',
-         df_bg_avg['neurons'], df_bg_avg['total_time'], 'r',
-         df_bg_avg['neurons'], df_bg_avg['exec_time'], 'r--')
+         df_bg_avg['neurons'], df_bg_avg['total_time'], 'r')
 plt.xlabel('# of neurons')
 plt.ylabel('t (s)')
 plt.grid(True)
@@ -40,12 +43,11 @@ curr_ylim = plt.ylim()
 plt.ylim((curr_ylim[0]*0.9, curr_ylim[1]*1.2))
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 plt.title('Execution Duration for 3.0 s Emulation at $\Delta t$ = 1e-4')
-plt.legend(['Neurodriver', 'Brian2GeNN', 'Brian2GeNN binary'], loc='upper left')
+plt.legend(['Neurodriver', 'Brian2GeNN'], loc='upper left')
 
 plt.subplot(212)
 plt.plot(df_nd_avg['synapses'], df_nd_avg['exec_time'], 'b',
-         df_bg_avg['synapses'], df_bg_avg['total_time'], 'r',
-         df_bg_avg['synapses'], df_bg_avg['exec_time'], 'r--')
+         df_bg_avg['synapses'], df_bg_avg['total_time'], 'r')
 plt.xlabel('# of synapses')
 plt.ylabel('t (s)')
 plt.grid(True)
@@ -54,3 +56,4 @@ curr_ylim = plt.ylim()
 plt.ylim((curr_ylim[0]*0.9, curr_ylim[1]*1.2))
 plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 plt.tight_layout()
+plt.savefig('neurodriver-benchmark.png', dpi=100)
