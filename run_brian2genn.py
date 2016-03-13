18142101d146450e6ffd3de81b6b9231c4418dce
Lev Givon <lev@columbia.edu>
2016-03-12 21:18:56 -0500

    Increase range of network sizes tested.

--- run_brian2genn.py
+++ run_brian2genn.py
@@ -4,7 +4,7 @@ import re
 
 import sarge
 
-for n in xrange(50, 3500, 50):
+for n in xrange(50, 4000, 50):
     c = sarge.capture_both('python -u brian2genn_demo.py -n %s %s %s' % (n, n, n))
     for line in c.stdout.readlines():
         if re.match('^\d.*', line):
